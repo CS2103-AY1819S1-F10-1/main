aff4f4e6670640e089f5efb4b1c92dc9d6680945
Jeremy Choo <ChooJeremy@Ymail.com>
2018-11-04 01:50:18 +0800

    Updated tests to test more

--- src/test/java/seedu/address/logic/commands/ProfilePicCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ProfilePicCommandTest.java
@@ -8,8 +8,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
+import seedu.address.model.person.ProfilePic;
 import seedu.address.model.person.User;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
@@ -21,13 +24,36 @@ public class ProfilePicCommandTest {
     private CommandHistory commandHistory = new CommandHistory();
 
     @Before
-    public void setUp() {
+    public void loginAsAdmin() {
         User.buildAdmin(User.ADMIN_DEFAULT_USERNAME, User.ADMIN_DEFUALT_PASSWORD);
         model.setLoggedInUser(User.getAdminUser());
     }
 
     @Test
+    public void no_file_error() throws Exception {
+        try {
+            ProfilePicCommand profilePicCommand = new ProfilePicCommand("random random random");
+            assert false;
+        } catch (ParseException pe) {
+            assert pe.getMessage().equals(String.format(ProfilePicCommand.INVALID_PATH_ERROR, "random random random"));
+        }
+    }
+
+    @Test
     public void build_command_success() throws Exception {
         ProfilePicCommand profilePicCommand = new ProfilePicCommand(SANDBOX_IMAGE);
+    }
+
+    @Test
+    public void command_admin_fail() throws Exception {
+        loginAsAdmin();
+
+        ProfilePicCommand profilePicCommand = new ProfilePicCommand(SANDBOX_IMAGE);
+        try {
+            profilePicCommand.execute(model, commandHistory);
+            assert false;
+        } catch (CommandException ce) {
+            assert ce.getMessage().equals(ProfilePicCommand.ADMIN_ERROR);
+        }
     }
 }
